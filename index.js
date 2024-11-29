319af8dbc2e23607c72a65dcc2142ce1e3aa5078
GarbledHamster <jrice@ourstuff.space>
2024-11-28 18:55:40 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+// src/index.js
+
+import { init as initNotes } from './modules/notes.js';
+import { init as initTasks } from './modules/tasks.js';
+import { init as initTimers } from './modules/timers.js';
+
+// Example: Initialize all tools on page load or based on user interaction
