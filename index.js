cc3a3d6ca70454052ee744a2006448bd11eb04ae
GarbledHamster <jrice@ourstuff.space>
2024-11-28 18:56:52 -0600

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-
-import { init as initNotes } from './modules/notes.js';
-import { init as initTasks } from './modules/tasks.js';
-import { init as initTimers } from './modules/timers.js';
-
-// Example: Initialize all tools on page load or based on user interaction
